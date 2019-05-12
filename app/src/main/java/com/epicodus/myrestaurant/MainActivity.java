15279ab28421e970bc628bc99a6d231d1b461799
dicksonparlma <dicksonparlma@gmail.com>
2019-05-12 17:10:34 +0300

    added a key from Yelp

--- app/src/main/java/com/epicodus/myrestaurant/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurant/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
   @BindView(R.id.findbutton) Button mFindRestaurantsButton;
   @BindView(R.id.appNameTextView) TextView mAppNameTextView;
   @BindView(R.id.locationInput) EditText mlocation;
@@ -24,20 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/font1.ttf");
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String location = mlocation.getText().toString();
-                Toast.makeText(MainActivity.this , "searching .." ,Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(MainActivity.this , RestaurantActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-            }
-        });
+//        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                String location = mlocation.getText().toString();
+//                Toast.makeText(MainActivity.this , "searching .." ,Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(MainActivity.this , RestaurantActivity.class);
+//                intent.putExtra("location", location);
+//                startActivity(intent);
+//            }
+//        });
+        mFindRestaurantsButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurantsButton) {
+            String location = mlocation.getText().toString();
+            Toast.makeText(MainActivity.this, "searching ..", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
 }
